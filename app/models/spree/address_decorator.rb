cad193356d0301ce59b6e1cf391165699a90739d
David Librera <davidlibrear@gmail.com>
2013-05-14 10:11:03 +0200

    added validation on length for codice fiscale e piva

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -2,4 +2,10 @@ Spree::Address.class_eval do
   attr_accessible :tax_code
 
   validates :tax_code, :presence => true
+
+  def check_tax_code
+    if tax_code.length != 16 || tax_code.length != 11
+      errors.add(:tax_code, "Codice Fiscale non valido")
+    end
+  end
 end
